007faeef71d23b20e309e2c446f4f58b5ec15a48
María José Vásquez <131217309+Majo077@users.noreply.github.com>
2023-05-04 17:43:03 -0400

    Add files via upload

---
+++ Practica1.py
@@ -0,0 +1,14 @@
+P1 = int(input('Ingrese el primer numero'))
+P2 = int(input('Ingrese el segundo numero'))
+P3 = int(input('Ingrese el tercer numero'))
+L1 = [P1,P2,P3]
+L1.sort()
+print(L1)
+
+S1 = input('Ingrese una cadena de caracteres en mayuscula')
+S1 = S1.casefold()
+Lista_1 = S1.split()
+print(Lista_1)
+
+
+
